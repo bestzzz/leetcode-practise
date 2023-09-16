c946abaa24b8691a11ddfcfae70c8b459b5fe612
zoe <707098011@qq.com>
2023-09-16 23:00:10 +0800

    feat: 删除链表的节点

---
+++ get-offer/test/link/JSOF_18.js
@@ -0,0 +1,47 @@
+/**
+ * 剑指 Offer 18. 删除链表的节点
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。返回删除后的链表的头节点。
+ * 
+ * 输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ */
+
+/**
+ * 解题思路：
+ * 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function(head, val) {
+  var dummy = new ListNode();
+  dummy.next = head;
+
+  var prev = dummy;
+  var cur = head;
+  while (cur) {
+    if (cur.val === val) {
+      prev.next = cur.next;
+      break;
+    }
+    prev = cur;
+    cur = cur.next;
+  }
+
+  return dummy.next;
+};
