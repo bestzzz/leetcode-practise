d894bbe2d4fc900dc6f59d43c0ee2df4822756fd
zuozhao <zuozhao@huohua.cn>
2023-10-12 19:09:39 +0800

    feat: 1.两数之和

---
+++ leetcode/2023/hot100/hash/1.twoSum.js
@@ -0,0 +1,46 @@
+/**
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+示例 2：
+
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+示例 3：
+
+输入：nums = [3,3], target = 6
+输出：[0,1]
+ */
+
+/**
+ * 解体思路：
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const cur = nums[i];
+    const diff = target - cur;
+    if (map[diff] || map[diff] === 0) {
+      return [map[diff], i];
+    };
+    map[cur] = i;
+  }
+  return [];
+};
+
+console.log(twoSum([2,7,11,15], 9));
